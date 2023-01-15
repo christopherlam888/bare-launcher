9e6d300fc2930e3aa7da927c6df87d7be1fc2f57
Christopher Lam <christopher.peter.lam@gmail.com>
2023-01-15 13:35:26 -0500

    Fix in portrait mode and update version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:device_apps/device_apps.dart';
 
 void main() {
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Homepage(),
